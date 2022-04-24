94e4bb8a96e6a0ca0487bf9bb56782008b35f5d6
A01770043 <102306632+A01770043@users.noreply.github.com>
2022-04-24 16:26:28 -0500

    Add files via upload
    portal administrador

---
+++ manage-mr.php
@@ -0,0 +1,69 @@
+<?php include('partials/menu.php') ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1> Manage Category</h1>
+    </div>
+</div>
+
+<br /> <br />
+
+
+<!-- Button to Add Admin -->
+<a href="#" class="btn-primary">Add Category</a>
+
+<br /> <br /> <br />
+
+<table class="tbl-full"> 
+        <tr>
+            <th>S.N</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+        </tr>
+
+        <tr>
+            <td>1.</td>
+            <td>LF</td>
+            <td>LF</td>
+            <td>
+                <a ref="#" class="btn-secondary">Update Admin</a>
+                <a ref="#" class="btn-danger">Delete Admin</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>2.</td>
+            <td>LF</td>
+            <td>LF</td>
+            <td>
+                <a ref="#" class="btn-secondary">Update Admin</a>
+                <a ref="#" class="btn-danger">Delete Admin</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>3.</td>
+            <td>LF</td>
+            <td>LF</td>
+            <td>
+                <a ref="#" class="btn-secondary">Update Admin</a>
+                <a ref="#" class="btn-danger">Delete Admin</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>4.</td>
+            <td>LF</td>
+            <td>LF</td>
+            <td>
+                <a ref="#" class="btn-secondary">Update Admin</a>
+                <a ref="#" class="btn-danger">Delete Admin</a>
+            </td>
+        </tr>
+</table>
+
+</div>
+</div>
+
+<?php include('partials/footer.php') ?>
